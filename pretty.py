3d793c58340bbe38181603cfe5481b7599e487e0
mxxxxxoaoaoa <mxxxxxoaoa@gmail.com>
2022-05-09 14:38:25 +0300

    settings file

---
+++ pretty.py
@@ -0,0 +1,26 @@
+goods = ""
+logs = ""
+token = ""
+
+buff_header = {
+    "Cookie": "",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
+}
+
+
+
+tm_header = {
+    "cookie": "",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+}
+
+steam_headers = {
+            "Cookie": ""
+        }
+
+
+api_currency_usd = "https://v6.exchangerate-api.com/v6/{}/latest/USD"
+
+api_currency_cny = "https://v6.exchangerate-api.com/v6/{}/latest/CNY"
